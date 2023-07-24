f483fd4678c5f4db6b7e89d0eb0c4e1bcff59385
Higino Silva <higino@creoate.com>
2023-07-24 21:50:48 +0100

    Using chat 3,5

--- index.js
+++ index.js
@@ -54,18 +54,14 @@ async function post1(prompt) {
 }
 
 async function post(prompt) {
-  let improvedPrompt = "Imagine you are Otis a digital personal assistant part of a SaaS product You can do tasks helping content creators and influencers manage social media, contents and followers. You can even lend them money as part of financing of their business. You can automate all the repetitive tasks that content creators need in order to build content faster and grow their audience and channels. All the tools they need are concentrated in Otis so they don't need to go anywhere else. Otis as a personal assistant can do everything using AI and automatically create or do what content creators need. If you are asked anything outside this scope please avoid responding. Try as much as possible to have people subscribe to the product." + 
+  let systemContent = "Imagine you are Otis a digital personal assistant part of a SaaS product You can do tasks helping content creators and influencers manage social media, contents and followers. You can even lend them money as part of financing of their business. You can automate all the repetitive tasks that content creators need in order to build content faster and grow their audience and channels. All the tools they need are concentrated in Otis so they don't need to go anywhere else. Otis as a personal assistant can do everything using AI and automatically create or do what content creators need. If you are asked anything outside this scope please avoid responding. Try as much as possible to have people subscribe to the product"
+  let improvedPrompt = systemContent + 
                 "User: " + prompt + "\nOtis: ";
 
   try {
     const openaiResponse = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      prompt: improvedPrompt,
-      temperature: 0.7,
-      max_tokens: 150,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6
+      messages: [{"role": "system", "content": systemContent}, {"role": "user", "content": prompt}]
     });
 
     const response = {
@@ -74,7 +70,7 @@ async function post(prompt) {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({response: openaiResponse.data.choices[0].text}),
+      body: JSON.stringify({response: completion.data.choices[0].message}),
     };
     
     return response;
