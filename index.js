173b2936d7288fd48f2e04ab1aeab95952a68315
Higino Silva <higino@creoate.com>
2023-07-24 21:36:36 +0100

    Start using gpt3.5 turbo model

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function post(prompt) {
 
   try {
     const openaiResponse = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: improvedPrompt,
       temperature: 0.7,
       max_tokens: 150,
